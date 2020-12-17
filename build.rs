8b1c8118643a29fb3fdaa646ad978a9b5d00075c
osk <tgv.alpha@gmail.com>
2020-12-17 04:43:23 +0200

    base.2

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
 
-    tonic_build::compile_protos("proto/report.proto")
+    tonic_build::compile_protos("../proto/report.proto")
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
